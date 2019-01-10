f0a171b7dce3890f6bc33d3025ff4917f4167a8e
xiaowen <952065841@qq.com>
2019-01-10 20:30:52 +0800

    add editor.md

--- controller/admin/base.go
+++ controller/admin/base.go
@@ -3,77 +3,67 @@ package admin
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"mime/multipart"
-	"os"
 	"webBlog/helper"
+	"github.com/qiniu/api.v7/auth/qbox"
+	"github.com/qiniu/api.v7/storage"
+	"golang.org/x/net/context"
+	"os"
 )
 
 type Base struct {
 }
-// 获取文件大小的接口
-type Size interface {
-	Size() int64
-}
-
-// 获取文件信息的接口
-type Stat interface {
-	Stat() (os.FileInfo, error)
-}
-
-// 构造返回值字段
-type PutRet struct {
-	Hash string `json:"hash"`
-	Key  string `json:"key"`
+// 自定义返回值结构体
+type MyPutRet struct {
+	Key    string
+	Hash   string
+	Fsize  int
+	Bucket string
+	Name   string
 }
 func (b *Base) Upload (c *gin.Context){
 	var err error
-	file, _, err := c.Request.FormFile("file")
+	file, err := c.FormFile("editormd-image-file")
 	if err == nil {
 		var key string
-		key, err = uploadFile(file)
+		var fileName = "static/uploads/"+file.Filename
+		err := c.SaveUploadedFile(file, fileName)
+		key, err = uploadFile(fileName)
+		config := helper.GetConfig()
 		if err == nil {
+			//删除文件
+			os.Remove(fileName)
 			c.JSON(http.StatusOK, gin.H{
-				"succeed": true,
-				"url":     system.GetConfiguration().QiniuFileServer + key,
+				"success": 1,
+				"url": config.GetValue("qiniu", "QiniuFileServer") + key ,
+				"message":"上传成功",
 			})
 			return
 		}
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"succeed": false,
+		"success": 0,
 		"message": err.Error(),
 	})
 }
-func uploadFile(file multipart.File) (string, error) {
+func uploadFile(file string) (string, error) {
 	config := helper.GetConfig()
 	qiniuAccessKey := config.GetValue("qiniu", "QiniuAccessKey")
 	qiniuSecretKey := config.GetValue("qiniu", "QiniuSecretKey")
 	qiniuBucket := config.GetValue("qiniu", "QiniuBucket")
 	// 创建一个Client
-	c := kodo.New(0, nil)
+	mac := qbox.NewMac(qiniuAccessKey, qiniuSecretKey)
 	// 设置上传的策略
-	policy := &kodo.PutPolicy{
+	putPolicy := storage.PutPolicy{
 		Scope: qiniuBucket,
-		//设置Token过期时间
-		Expires: 3600,
+		Expires:7200,
+		ReturnBody: `{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}`,
 	}
 	// 生成一个上传token
-	token := c.MakeUptoken(policy)
-	// 构建一个uploader
-	zone := 0
-	uploader := kodocli.NewUploader(zone, nil)
-
-	var size int64
-	if statInterface, ok := file.(Stat); ok {
-		fileInfo, _ := statInterface.Stat()
-		size = fileInfo.Size()
-	}
-	if sizeInterface, ok := file.(Size); ok {
-		size = sizeInterface.Size()
-	}
-
-	var ret PutRet
-	err := uploader.PutWithoutKey(nil, &ret, token, file, size, nil)
+	upToken := putPolicy.UploadToken(mac)
+	cfg := storage.Config{}
+	formUploader := storage.NewFormUploader(&cfg)
+	ret := MyPutRet{}
+	err := formUploader.PutFileWithoutKey(context.Background(), &ret, upToken, file, nil)
 	if err != nil {
 		return "", err
 	}
--- go.mod
+++ go.mod
@@ -1,18 +1,33 @@
 module webBlog
 
 require (
+	cloud.google.com/go v0.34.0 // indirect
 	github.com/Unknwon/goconfig v0.0.0-20181105214110-56bd8ab18619
+	github.com/denisenkom/go-mssqldb v0.0.0-20181014144952-4e0d7dc8888f // indirect
+	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
 	github.com/gin-contrib/sessions v0.0.0-20190101140330-dc5246754963
 	github.com/gin-gonic/gin v1.3.0
 	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/gofrs/uuid v3.1.0+incompatible // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/google/go-cmp v0.2.0 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
 	github.com/jinzhu/gorm v1.9.2
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
-	github.com/kr/pty v1.1.3 // indirect
+	github.com/jinzhu/now v0.0.0-20181116074157-8ec929ed50c3 // indirect
+	github.com/jtolds/gls v4.2.1+incompatible // indirect
+	github.com/lib/pq v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	github.com/qiniu/api.v7 v7.2.5+incompatible
+	github.com/qiniu/x v7.0.8+incompatible // indirect
+	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
+	github.com/smartystreets/goconvey v0.0.0-20181108003508-044398e4856c // indirect
 	github.com/steambap/captcha v1.3.0
-	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190103213133-ff983b9c42bc // indirect
 	golang.org/x/image v0.0.0-20181116024801-cd38e8056d9b // indirect
-	golang.org/x/net v0.0.0-20190108225652-1e06a53dbb7e // indirect
+	golang.org/x/net v0.0.0-20190108225652-1e06a53dbb7e
 	golang.org/x/sys v0.0.0-20190108104531-7fbe1cd0fcc2 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	qiniupkg.com/x v7.0.8+incompatible // indirect
 )
