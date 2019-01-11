66d5e97a4b0e8136a12d274b20d735106942bdc5
xiaowen <952065841@qq.com>
2019-01-11 12:00:35 +0800

    add article

--- controller/admin/article.go
+++ controller/admin/article.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"webBlog/helper"
 	"net/http"
+	"webBlog/model"
+	"fmt"
 )
 
 type Article struct {
@@ -12,14 +14,46 @@ type Article struct {
 func (t *Article) Index (c *gin.Context){
 
 }
-
+type ArticleData struct {
+	Title string `form:"name" binding:"required"`
+	Cid int `form:"cid" binding:"required"`
+	Description string `form:"description"`
+	Tags string `form:"tags"`
+	Cover string `form:"cover" binding:"required"`
+	Content string`form:"content"`
+}
 func (t *Article) Add (c *gin.Context){
 	if helper.IsGet(c) {
-		//获取所有的顶级分类
+		//获取所有的分类
+		categorys := model.GetTree()
 		errorMsg := helper.GetFlash(c, "errorMsg")
 		c.HTML(http.StatusOK, "admin/article/add.html",gin.H{
 			"errorMsg":errorMsg,
+			"categorys":categorys,
 		})
+	} else if helper.IsPost(c) {
+		var articleData ArticleData
+		if err := c.ShouldBind(&articleData); err == nil {
+			article := model.Article{
+				Title:articleData.Title,
+				Cid:articleData.Cid,
+				Description:articleData.Description,
+				Tags:articleData.Tags,
+				Cover:articleData.Cover,
+				Content:articleData.Content,
+			}
+			err := model.DB.Create(&article).Error
+			if err == nil {
+				fmt.Println(article.ID)
+				model.HandelTagAdd(articleData.Tags)
+				helper.SetFlash(c, "errorMsg", "添加成功 ！")
+			} else {
+				helper.SetFlash(c, "errorMsg", "添加失败 ！")
+			}
+		} else {
+			helper.SetFlash(c, "errorMsg", "缺少参数 ！")
+		}
+		c.Redirect(http.StatusFound, "/admin/article/add")
 	}
 }
 
--- model/tag.go
+++ model/tag.go
@@ -1,7 +1,69 @@
 package model
 
+import (
+	"strings"
+)
+
 type Tag struct {
 	BaseModel
 	Name string `gorm:"size:50;not null"` //名称
 	ArticleNum int `gorm:"default:0;not null"` //数量
+}
+
+func HandelTagAdd(tags string)  {
+	//新增
+	if tags == "" {
+		return
+	}
+	//转换tags
+	tags = strings.Replace(tags,"，", "," , -1)
+	tagsSlice := strings.Split(tags, ",")
+	tagsMap := getTags()
+	for _, tag := range tagsSlice {
+		isHas := false
+		for oldTag, _ := range tagsMap {
+			if oldTag == tag {
+				isHas = true
+			}
+		}
+		if isHas {
+			//加1
+			var tagInfo Tag
+			DB.Where("name = ?", tag).First(&tagInfo)
+			tagInfo.ArticleNum += 1
+			DB.Save(&tagInfo)
+		} else {
+			//新增
+			DB.Create(&Tag{Name:tag,ArticleNum:1})
+		}
+	}
+}
+func HandelTagDel(tags string) {
+	tagsSlice := strings.Split(tags, ",")
+	for _, tag := range tagsSlice {
+		//减1
+		var tagInfo Tag
+		DB.Where("name = ?", tag).First(&tagInfo)
+		if tagInfo.ArticleNum > 1 {
+			tagInfo.ArticleNum -= 1
+			DB.Save(&tagInfo)
+		} else {
+			DB.Delete(&tagInfo)
+		}
+	}
+}
+func HandelTagEdit(oldTags string, tags string) {
+	HandelTagDel(oldTags)
+	HandelTagAdd(tags)
+}
+func getTags() map[string]int{
+	var tags []*Tag
+	DB.Find(&tags)
+	tagsMap := make(map[string]int)
+	if len(tags) > 0 {
+		for _, v := range tags {
+			tagsMap[v.Name] = v.ArticleNum
+		}
+	}
+	return tagsMap
 }
